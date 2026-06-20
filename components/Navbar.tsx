69a27a3d03bee2f43cb9d58a1eadb023524bcd01
kibria <mdkibria.dev@gmail.com>
2026-06-20 18:29:16 +0600

    fixed mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -24,32 +24,35 @@ const Navbar = () => {
                     </Link>
                 </div>
             </nav>
+            <div className='fixed md:hidden top-0 left-0 right-0 z-50 h-16 flex items-center justify-between text-primary-text px-5 bg-white/90 backdrop-blur-lg border-b border-surface/20'>
+                <Image src="/logo.png" alt="Logo" width={200} height={50} className="w-fit h-10" />
+            </div>
             <nav className='fixed md:hidden bottom-0 left-0 right-0 z-50 h-16 bg-white/90 backdrop-blur-lg border-b border-surface/20'>
-                <ul className='w-full flex justify-between items-center px-5 h-full'>
-                    <li>
+                <ul className='w-full grid grid-cols-5 justify-center items-center px-5 h-full'>
+                    <li className='mx-auto'>
                         <Link href="/" className='text-sm w-full flex items-center gap-0 text-primary-text/80 hover:text-primary rounded-none transition-all duration-150 flex-col'>
                             <Home size={22} className='font-normal' />
                             <span className='font-normal'>Feed</span>
                         </Link>
                     </li>
-                    <li>
+                    <li className='mx-auto'>
                         <Link href="/" className='text-sm w-full flex items-center gap-0 text-primary-text/80 hover:text-primary rounded-none transition-all duration-150 flex-col'>
                             <Search size={22} className='font-normal' />
                             <span className='font-normal'>Explore</span>
                         </Link>
                     </li>
-                    <li>
+                    <li className='mx-auto'>
                         <Link href="/" className='text-sm flex items-center justify-center gap-0 text-white hover:text-primary rounded-full transition-all duration-150 flex-col bg-primary h-11 w-11'>
                             <Plus size={22} className='font-normal' />
                         </Link>
                     </li>
-                    <li>
+                    <li className='mx-auto'>
                         <Link href="/" className='text-sm w-full flex items-center gap-0 text-primary-text/80 hover:text-primary rounded-none transition-all duration-150 flex-col'>
                             <Astroid size={22} className='font-normal' />
                             <span className='font-normal'>Assistant</span>
                         </Link>
                     </li>
-                    <li>
+                    <li className='mx-auto'>
                         <Link href="/" className='text-sm w-full flex items-center gap-0 text-primary-text/80 hover:text-primary rounded-none transition-all duration-150 flex-col'>
                             <User size={22} className='font-normal' />
                             <span className='font-normal'>Profile</span>
