f3549ab44babd25c8f2216f88e4a8754b3dc27b1
kibria <mdkibria.dev@gmail.com>
2026-06-20 19:42:32 +0600

    fixed some ui issues

--- components/AiButton.tsx
+++ components/AiButton.tsx
@@ -4,7 +4,7 @@ import { Sparkles } from 'lucide-react'
 
 const AiButton = () => {
   return (
-    <Button className='rounded-full w-14 h-14 fixed bottom-10 right-10 bg-primary hover:bg-primary/80'>
+    <Button className='rounded-full w-14 h-14 fixed bottom-20 md:bottom-10 right-5 md:right-10 bg-primary hover:bg-primary/80 z-50'>
         <Sparkles className='h-20 w-20 text-white' />
     </Button>
   )
