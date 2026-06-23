cb7778cea93ed23a1b761855e3a085960e2cbe5f
kibria <mdkibria.dev@gmail.com>
2026-06-24 01:27:34 +0600

    updated middleware

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -1,23 +1,42 @@
+import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
-import { createSupabaseServerClient } from './server-client'
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({ request })
 
-  const supabase = await createSupabaseServerClient();
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll()
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value)
+          )
+          supabaseResponse = NextResponse.next({ request })
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options)
+          )
+        },
+      },
+    }
+  )
 
   const { data: { user } } = await supabase.auth.getUser()
 
-  // const protectedPaths = ['/profile']
-  // const isProtected = protectedPaths.some((p) =>
-  //   request.nextUrl.pathname.startsWith(p)
-  // )
+  const protectedPaths = ['/profile']
+  const isProtected = protectedPaths.some((p) =>
+    request.nextUrl.pathname.startsWith(p)
+  )
 
-  // if (isProtected && !user) {
-  //   const url = request.nextUrl.clone()
-  //   url.pathname = '/login'
-  //   return NextResponse.redirect(url)
-  // }
+  if (isProtected && !user) {
+    const url = request.nextUrl.clone()
+    url.pathname = '/login'
+    return NextResponse.redirect(url)
+  }
 
   return supabaseResponse
 }
