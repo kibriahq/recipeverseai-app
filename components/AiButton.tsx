4294f3d952502ffe5a43fb4de32e04904bc0399a
kibria <mdkibria.dev@gmail.com>
2026-06-23 20:22:19 +0600

    improved

--- components/AiButton.tsx
+++ components/AiButton.tsx
@@ -8,7 +8,7 @@ const AiButton = () => {
   const pathname = usePathname();
 
   return (
-    <Button asChild className={`rounded-full w-14 h-14 fixed bottom-20 md:bottom-10 right-5 md:right-10 bg-primary hover:bg-primary/80 z-50 ${pathname === '/ai-assistant' ? 'hidden' : ''}`}>
+    <Button asChild className={`rounded-full w-14 h-14 fixed bottom-20 md:bottom-10 right-5 md:right-10 bg-primary hover:bg-primary/80 z-50 ${pathname === '/ai-assistant' || pathname.includes('/recipes') ? 'hidden' : ''}`}>
       <Link href='/ai-assistant'>
         <Sparkles className='h-7 w-7 text-white' />
       </Link>
